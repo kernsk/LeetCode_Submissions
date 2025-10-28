96ee97ed6ca1b70fd99da3dd0ae91b91c974f549
Kevin Kerns <116226387+kernsk@users.noreply.github.com>
2025-10-28 16:13:55 -0700

    Update 2487_optimal_partition_of_string.py - LeetCode Problem 2487

--- 2487_optimal_partition_of_string.py
+++ 2487_optimal_partition_of_string.py
@@ -6,7 +6,8 @@
 # partition. If a char is already in the set, increment count that was initialized
 # to 1, clear set, then add the char to it. Return count. Time complexity is O(n)
 # to traverse the string. Membership checks in an unordered set are done in
-# constant time. Space complexity is O(n) if every letter is unique.
+# constant time. Space complexity is O(1) because the alphabet has a fixed amount
+# of letters – it does not scale with n.
 
 class Solution:
     def partitionString(self, s: str) -> int:
